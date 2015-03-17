127e1bec7189d397cb873766f2b5f8add89c7bd3
Makoto Ishida <makotoishida@gmail.com>
2015-03-16 19:35:00 -1000

    Gridのカスタマイズサンプル追加。 RowTemplateを使う。

---
+++ AspNet/KendoUIWeb/Scripts/App/Samples/grid5_2.js
@@ -0,0 +1,55 @@
+﻿var App = App || {};
+
+(function () {
+    'option strict';
+
+    var vm;
+    App.ViewModel = vm= new kendo.data.ObservableObject({
+        element: "#app",
+
+        criteria: { category_id: null, supplier_id: null, product_name:"", active_only: true },
+
+        categoryList : new kendo.data.DataSource({
+            transport: {
+                read: {
+                    url: "http://localhost:50194/api/categories",
+                    dataType: "json"
+                }
+            }
+        }), 
+
+        productList: new kendo.data.DataSource({
+            transport: {
+                read: {
+                    url: "http://localhost:50194/api/ProductsPaged",
+                    dataType: "json",
+                    data: function () {
+                        return vm.get("criteria").toJSON();
+                    }
+                }
+            },
+            schema: {
+                model: { id: "ProductID" },
+                data: "list",
+                total: "total"
+            },
+            serverPaging: true,
+            pageSize: 10
+        }),
+
+        refreshProductList: function () {
+            vm.productList.read();
+        }
+
+    });
+
+    kendo.bind(vm.element, vm);
+
+    //コンボボックスのリスト部分の幅をセット。
+    $(vm.element).find("input[data-role='combobox']").each(function (index, item) {
+        var combo = $(item).data("kendoComboBox");
+        if (combo) combo.list.width(300);
+    });
+
+})();
+
